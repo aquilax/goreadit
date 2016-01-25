a63fe015f97502715532edede4617213befe3b58
aquilax <aquilax@gmail.com>
2016-01-25 06:16:37 +0100

    Added tokenizer

--- readit.go
+++ readit.go
@@ -9,5 +9,9 @@ func NewReadIt(config *Config) *ReadIt {
 }
 
 func (r *ReadIt) Run() error {
+	tokenizer := NewTokenizer()
+	if err := tokenizer.Process(r.config.FileName); err != nil {
+		return err
+	}
 	return nil
 }
---
+++ tokenizer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/neurosnap/sentences"
+)
+
+type Tokenizer struct {
+	sentences []*sentences.Sentence
+}
+
+func NewTokenizer() *Tokenizer {
+	return &Tokenizer{}
+}
+
+func (t *Tokenizer) Process(fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	return t.process(file)
+}
+
+func (t *Tokenizer) process(reader io.Reader) error {
+	tokenizer := sentences.NewSentenceTokenizer(nil)
+	text, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+	t.sentences = tokenizer.Tokenize(string(text))
+	return nil
+}
