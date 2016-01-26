b898d21be77470fdc09840a523bf86479b2e93d7
aquilax <aquilax@gmail.com>
2016-01-26 07:44:15 +0100

    Looks nice

--- termbox.go
+++ termbox.go
@@ -1,13 +1,13 @@
 package main
 
-import (
-	"github.com/nsf/termbox-go"
-)
+import "github.com/nsf/termbox-go"
 
 type Termbox struct {
-	input  *Input
-	width  int
-	height int
+	input   *Input
+	width   int
+	height  int
+	centerX int
+	centerY int
 }
 
 func NewTermbox() *Termbox {
@@ -22,7 +22,7 @@ func (t *Termbox) Run() error {
 		panic(err)
 	}
 	defer termbox.Close()
-	termbox.SetOutputMode(termbox.Output256)
+	termbox.SetOutputMode(termbox.OutputNormal)
 	termbox.SetInputMode(termbox.InputAlt)
 	t.updateSize(termbox.Size())
 	t.input.Start()
@@ -50,6 +50,9 @@ mainloop:
 func (t *Termbox) updateSize(w, h int) {
 	t.width = w
 	t.height = h
+	t.centerX = t.width / 2
+	t.centerY = t.height / 2
+
 }
 
 func (t *Termbox) update(ev termbox.Event) {
@@ -58,6 +61,32 @@ func (t *Termbox) update(ev termbox.Event) {
 
 func (t *Termbox) draw() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	termbox.SetCell(3, 4, 'a', termbox.ColorWhite, termbox.ColorDefault)
+	t.renderWireframe()
+	t.renderWord("килиманджаро")
 	termbox.Flush()
+}
+
+func (t *Termbox) renderWireframe() {
+	for y := t.centerY - 2; y <= t.centerY+2; y++ {
+		for x := 0; x < t.width; x++ {
+			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.ColorWhite)
+		}
+	}
+	termbox.SetCell(t.centerX, t.centerY-1, '|', termbox.ColorRed, termbox.ColorWhite)
+	termbox.SetCell(t.centerX, t.centerY+1, '|', termbox.ColorRed, termbox.ColorWhite)
+}
+
+func (t *Termbox) renderWord(word string) {
+	//length := utf8.RuneCountInString(word)
+	breakingPoint := 2 // TODO: calculate that
+	i := 0
+	for _, runeValue := range word {
+		x := t.centerX - breakingPoint + i
+		color := termbox.ColorBlack
+		if x == t.centerX {
+			color = termbox.ColorRed
+		}
+		termbox.SetCell(x, t.centerY, runeValue, color, termbox.ColorWhite)
+		i++
+	}
 }
